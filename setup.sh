41fce3db41109641e5039e90c1da9fe8f22a6c05
shridhar-sable <100476671+shridhar-sable@users.noreply.github.com>
2022-07-24 23:25:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"sahilrajpal121@gmail.com\"\n\
+email = \"21f1003511@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
